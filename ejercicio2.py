0c08abdf795ff5c7feb9725a995c32d04d281acc
Tanoni Brenda <brendayohenatanoni@gmail.com>
2023-08-19 01:06:07 -0300

    ejercicio 2

---
+++ ejercicio2.py
@@ -0,0 +1,7 @@
+# Desarrolar un programa que permita ingresar 2 numeros, cada uno en una variable. Mostrar el resultado de la suma de los mismos
+
+digito1 = float(input("ingresa un numero "))
+digito2 = float(input("ingresa otro numero "))
+
+resultado = digito1 + digito2
+print("La suma de", digito1, "y", digitp2, "es:", resultado)
